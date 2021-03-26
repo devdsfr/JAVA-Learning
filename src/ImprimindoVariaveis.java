91985f9c46f940543bf62f7f1ce9f4a0a04a953f
Daniel Henrique Jorge Ramos <daniel.henrique@totvs.com.br>
2021-03-25 22:49:56 -0300

    02 - RelatorioEmpregado

---
+++ src/ImprimindoVariaveis.java
@@ -0,0 +1,10 @@
+public class ImprimindoVariaveis {
+        public static void main(String[] args) {
+            String nome = "Daniel Ramos";
+            String endereco = "Rua Aluminio";
+            String telefone = "(62) 994345604";
+
+            System.out.println("O "+ nome +" domiciliado no endereço " + endereco + " e telefone "
+                               + telefone+ " não possui nenhum tipo de pendencia");
+    }
+}
---
+++ src/RelatorioEmpregado.java
@@ -0,0 +1,13 @@
+public class RelatorioEmpregado {
+    public static void main(String[] args) {
+        String nome = "Daniel Ramos";
+        String endereco = "Rua Alumínio";
+        String telefone = "(62) 994345604";
+        String sexo = "M";
+        int idade = 29;
+        String estadoCivil = "Solteiro";
+        Double salario = 4500.00 ;
+        System.out.println("O trabalhador "+nome+ " de sexo "+sexo+ " , idade "+idade+ " , estado civil "+estadoCivil+ " e salário "+salario+
+                           "encontra-se empregrado neste estabelecimento" );
+    }
+}
