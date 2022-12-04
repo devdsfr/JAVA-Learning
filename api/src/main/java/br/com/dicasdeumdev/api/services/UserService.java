475b074d32e0d51a568b2b5f0e9405cbf15c3551
Daniel Ramos <44585550+devdsfr@users.noreply.github.com>
2022-12-04 07:45:15 -0300

    Create method create

--- api/src/main/java/br/com/dicasdeumdev/api/services/UserService.java
+++ api/src/main/java/br/com/dicasdeumdev/api/services/UserService.java
@@ -1,6 +1,7 @@
 package br.com.dicasdeumdev.api.services;
 
 import br.com.dicasdeumdev.api.domain.User;
+import br.com.dicasdeumdev.api.domain.dto.UserDTO;
 
 import java.util.List;
 
@@ -8,5 +9,6 @@ public interface UserService {
 
     User findById(Long id);
     List<User> findAll();
+    User create(UserDTO obj);
 
 }
