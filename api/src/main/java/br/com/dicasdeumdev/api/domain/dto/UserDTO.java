17dc6fbb37d509a9e5fecfb03018923d98ca1513
Daniel Ramos <44585550+devdsfr@users.noreply.github.com>
2022-12-04 07:17:35 -0300

    Create class userDTO

---
+++ api/src/main/java/br/com/dicasdeumdev/api/domain/dto/UserDTO.java
@@ -0,0 +1,21 @@
+package br.com.dicasdeumdev.api.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter @Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private Long id;
+    private String name;
+
+    private String email;
+
+    @JsonIgnore
+    private String password;
+}
